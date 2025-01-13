fd48398da3ba02529c2c3432022a0321da187299
Maynor Aguilera <maynoraguileraosorto@gmail.com>
2025-01-13 10:36:49 -0600

    first commit

---
+++ 1.Hola mundo.cpp
@@ -0,0 +1,8 @@
+#include  <iostream>
+using namespace std ;
+
+int main(){
+    cout<<"Hola mundol";
+    return 0;
+}
+
---
+++ 2.SumaSimple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*Prueba de comentarios prro*/
+int main(){
+    int numero_1;
+    int numero_2;
+
+    cout<<"ingrese los numeros a calcular: ";
+    cin>>numero_1;
+    cin>>numero_2;
+
+    cout<<"Suma: "<< numero_1+numero_2<<endl;
+    cout<<"Resta: "<< numero_1-numero_2<<endl;
+    cout<<"Multiplicacion: "<< numero_1*numero_2<<endl;
+    cout<<"Division: "<< numero_1/numero_2<<endl;
+
+    return 0;
+}
